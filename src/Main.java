2786ee7c346ce47e55370455a7b46e365e1a5285
DeimosC1 <62855417+DeimosC1@users.noreply.github.com>
2020-12-17 16:58:20 +0100

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-    User username = new User("Bruh","aha");
+    Ship destroyer = new Builder("Destroyer",5 ,2,).hitpoints(100).damage(50);
+        Ship PTB = new Builder("Patrol Boat",2 ,1,).hitpoints(20).damage(20);
 
     }
 
